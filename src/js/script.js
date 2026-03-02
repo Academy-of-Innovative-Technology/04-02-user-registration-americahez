2d37c58bb3b92259c606960cbeb0fbdb49ab6a6b
America <AMericah8@nycstudents.net>
2026-03-02 14:08:47 -0500

    done

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,18 @@
 document.querySelector(".btn-primary").addEventListener("click", function (event) {
 
-    const firstName = document.querySelector("");
+    const first = document.querySelector(".first");
+    var last = document.querySelector(".last");
+    var email = document.querySelector(".email");
+    var acc = document.querySelector(".form-control-input:checked");
 
+    var user = {
+        firstName: first,
+        lastName: last,
+        userEmail: email,
+        accType: acc
+    };
+    localStorage.setItem("user", JSON.stringify(user));
+
+    var u = localStorage.getItem("user");
+    console.log(JSON.parse(u));
 });
